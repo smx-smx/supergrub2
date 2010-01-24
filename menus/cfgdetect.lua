203216ac50072078f2619d2c63523918d453c4cf
Jordan Uggla <Jordan.Uggla@gmail.com>
2010-01-23 20:30:52 -0800

    Fix copyright in cfgdetect.lua

--- menus/cfgdetect.lua
+++ menus/cfgdetect.lua
@@ -1,20 +1,20 @@
 #!lua
+-- This file is part of Super GRUB2 Disk.
 --
--- Copyright (C) 2009  Jordan Uggla
--- Copyright (C) 2009  Free Software Foundation, Inc.
+-- Copyright (C) 2009, 2010 Jordan Uggla
 --
--- GRUB is free software: you can redistribute it and/or modify
+-- Super GRUB Disk is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
 -- the Free Software Foundation, either version 3 of the License, or
 -- (at your option) any later version.
 --
--- GRUB is distributed in the hope that it will be useful,
+-- Super GRUB Disk is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 -- GNU General Public License for more details.
 --
 -- You should have received a copy of the GNU General Public License
--- along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
+-- along with Super GRUB Disk.  If not, see <http://www.gnu.org/licenses/>.
 --
 
 -- Attempts to find, and add menu entries to load, any grub.cfg files
